145892160648e72e326b14d29363441ffe8e3b5f
chitresh99 <chitreshpoojary990@gmail.com>
2025-07-04 14:10:31 +0530

    feat(upload): add upload

--- app/main.go
+++ app/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+)
+
+func uploadFile(w io.Writer, bucket, object string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %w", err)
+	}
+	defer client.Close()
+
+	// Open local file.
+	f, err := os.Open(object)
+	if err != nil {
+		return fmt.Errorf("os.Open: %w", err)
+	}
+	defer f.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	o := client.Bucket(bucket).Object(object)
+	o = o.If(storage.Conditions{DoesNotExist: true})
+	wc := o.NewWriter(ctx)
+	if _, err = io.Copy(wc, f); err != nil {
+		return fmt.Errorf("io.Copy: %w", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %w", err)
+	}
+	fmt.Fprintf(w, "Blob %v uploaded.\n", object)
+	return nil
+}
 
 func main() {
-	fmt.Println("Add the api end points here")
+	bucket := "gammo"
+	object := "notes.txt"
+
+	if err := uploadFile(os.Stdout, bucket, object); err != nil {
+		fmt.Fprintf(os.Stderr, "uploadFile: %v\n", err)
+		os.Exit(1)
+	}
 }
